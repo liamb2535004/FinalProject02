17524272d9c0bea4d25ec31d7379c860a64c37f6
liamb2535004 <lbohdjal@gmail.com>
2026-05-10 15:45:30 -0400

    validation class with incomplete isValidISBN method

---
+++ src/main/java/org/liamb/util/Validation.java
@@ -0,0 +1,8 @@
+package org.liamb.util;
+
+public class Validation {
+    public static boolean isValidISBN(String isbn) {
+        //TODO write method, unit test and exception handling
+        //make sure to accept ISBN 10 and 13
+    }
+}
