ac24b08ccec0635e258798269273ca5fc1a22442
liamb2535004 <lbohdjal@gmail.com>
2026-05-10 15:21:24 -0400

    Magazine class

---
+++ src/main/java/org/liamb/domain/Magazine.java
@@ -0,0 +1,25 @@
+package org.liamb.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Magazine extends Item{
+    private String issueNumber;
+    private String publisher;
+
+    public Magazine(String title, String issueNumber, String publisher) {
+        super(title);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+
+    public Magazine(String id, ItemStatus status, String title, String issueNumber, String publisher) {
+        super(id, status, title);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+}
