92ac0aeaef729691e81a5226b9a4c0644d888b9d
liamb2535004 <lbohdjal@gmail.com>
2026-05-10 17:30:20 -0400

    LibraryManagementSystem without methods

---
+++ src/main/java/org/liamb/service/LibraryManagementSystem.java
@@ -0,0 +1,33 @@
+package org.liamb.service;
+
+import org.liamb.domain.Item;
+import org.liamb.domain.User;
+
+import java.util.List;
+
+public class LibraryManagementSystem {
+    private static List<User> users;
+    private static List<Item> items;
+
+    public void loadCsvFiles() {
+        //TODO write method, unit test and exception handling
+    }
+
+    public Item searchItemRecursive(String query) {
+        //TODO write method, unit test and exception handling
+    }
+
+    public Item searchItemStream(String query) {
+        //TODO write method, unit test and exception handling
+    }
+
+    public void sortUsers() {
+        //TODO write method, unit test and exception handling
+        //bubble
+    }
+
+    public void sortItems() {
+        //TODO write method, unit test and exception handling
+        //Insertion
+    }
+}
