c77a30687a91febf2b338930ff6d1070a28dc656
liamb2535004 <lbohdjal@gmail.com>
2026-05-10 15:18:38 -0400

    DVD class

---
+++ src/main/java/org/liamb/domain/DVD.java
@@ -0,0 +1,26 @@
+package org.liamb.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class DVD extends Item{
+    private String director;
+    private int duration;
+
+    public DVD(String title, String director, int duration) {
+        super(title);
+        this.director = director;
+        this.duration = duration;
+    }
+
+    public DVD(String id, ItemStatus status, String title, String director, int duration) {
+        super(id, status, title);
+        this.director = director;
+        this.duration = duration;
+    }
+}
